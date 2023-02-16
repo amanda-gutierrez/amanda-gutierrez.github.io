3ead45488729a35333df0ff377d22fa31bf16782
af-m4rkly <70079018+af-m4rkly@users.noreply.github.com>
2023-02-15 17:17:00 -0800

    added files to test server

---
+++ projects/p5/puzzle.js
@@ -0,0 +1,29 @@
+
+
+let puzzleSketch = function(p){
+    let img;
+    p.onload = function(){
+        img = p.loadImage('images/redTrees.jpg');
+    } 
+    const canvasSize = 500;
+    p.setup = function(){
+        p.createCanvas(canvasSize, canvasSize + 50);
+        this.background(220);
+
+        //image(img, 0, 0);
+
+        let button = p.createButton("start");
+        const buttonWidth = 55;
+        const buttonHeight = 25;
+        button.size(buttonWidth, buttonHeight);
+        button.position(canvasSize / 2 - buttonWidth / 2, 25 + canvasSize);
+      }
+      p.draw = function() {
+      }
+
+}
+
+let p5Puzzle = new p5(puzzleSketch, "puzzle");
+
+
+
